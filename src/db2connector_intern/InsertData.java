1908f12abf7f3ecd239ae4a86303d0b9c74bfb5f
davidwoetzer <davidwoetzer@gmail.com>
2014-05-12 23:05:31 +0200

    Update InsertData.java

--- src/db2connector_intern/InsertData.java
+++ src/db2connector_intern/InsertData.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package db2connector_intern;
 
 
@@ -18,7 +14,7 @@ import java.sql.Statement;
 
 /**
  *
- * @author dawo1
+ * @author David Woetzer
  */
 public class InsertData {
 
