282c0a9cdb0763c709a072c91a9666fa3519053c
davidwoetzer <davidwoetzer@gmail.com>
2014-05-12 23:05:05 +0200

    Update FetchData.java

--- src/db2connector_intern/FetchData.java
+++ src/db2connector_intern/FetchData.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package db2connector_intern;
 
 import static db2connector_intern.Main.jdbcClassName;
@@ -19,7 +15,7 @@ import java.util.List;
 
 /**
  *
- * @author dawo1
+ * @author David Woetzer
  */
 public class FetchData {
 
